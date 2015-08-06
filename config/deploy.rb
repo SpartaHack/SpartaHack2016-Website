92381cb532a63adb52f163ab1b3cc6b4649cc0d4
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-08-06 15:40:13 -0400

    why is this dumb 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,8 +49,8 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
+      unless `git rev-parse HEAD` == `git rev-parse origin/development`
+        puts "WARNING: HEAD is not the same as origin/development"
         puts "Run `git push` to sync changes."
         exit
       end
