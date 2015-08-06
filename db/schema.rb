94c40afedda2daece45a637dc9cdc886cd4de0d4
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-08-06 13:51:13 -0400

    go

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150626012638) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
