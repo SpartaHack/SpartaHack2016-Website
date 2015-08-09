2c2d415236c8af20f9cd5c4d7c31bba503a3af47
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-08-08 22:16:01 -0400

    auto deploy 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
+set :use_sudo, false
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
@@ -19,8 +20,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :use_sudo, false
-set :ssh_options,     { forward_agent: true}
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
