0a70510739782ca67e510a71292fb7ca6fd458a3
Bogdan Pozderca <Bogdan@bogdans-mbp.bluemedora.localnet>
2015-08-11 15:21:10 -0400

    removed header.js, added scroll effects to nav bar. working on sponsor pdf

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -16,13 +16,51 @@ $('.anchorLink').click(function(){
   return false;
 });
 
-$("#header").headroom({
-  "offset": 205,
-  "tolerance": 5,
-  "classes": {
-    "initial": "animated",
-    "pinned": "slideDown",
-    "unpinned": "slideUp"
+// $("#header").headroom({
+//   "offset": 205,
+//   "tolerance": 5,
+//   "classes": {
+//     "initial": "animated",
+//     "pinned": "slideDown",
+//     "unpinned": "slideUp"
+//   }
+// });
+
+
+var menu = [
+  {"scroll_to": "#home-nav", "elem": $("#hero")},
+  {"scroll_to": "#faq-nav", "elem": $("#faq")},
+  {"scroll_to": "#contact-nav", "elem": $("#contact")},
+];
+
+var current = "home";
+
+$(".svg-wrapper").hover( 
+  function () { 
+    $(".svg-wrapper").removeClass("active");
+    $(this).addClass("active"); 
+  },
+  function () { 
+    $(".svg-wrapper").removeClass("active");
+    $(current).addClass("active"); 
+  }
+
+);
+
+$(window).scroll(function() {
+  var halfHeight = $(this).scrollTop() + ($(this).height() / 1.7);
+
+  for(var i = 0; i < menu.length; i++) {
+    var topOffset = menu[i]["elem"].offset().top;
+    var height = menu[i]["elem"].height();
+
+    if(halfHeight >= topOffset && halfHeight <= (topOffset + height) && current != menu[i]["scroll_to"]) {
+      var scroll_to = menu[i]["scroll_to"];
+      // change the selected menu element
+      $(".svg-wrapper").removeClass("active");
+      $(scroll_to).addClass("active");
+      current = scroll_to;
+    }
   }
 });
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ActionController::Base
   def index
   end
+
+	def sponsor
+		prospectus = File.join(Rails.root, "lib/prospectus.pdf")
+		#send_file(prospectus, :filename => "prospectus.pdf", :disposition => 'inline', :type => "application/pdf")
+	end
 end
