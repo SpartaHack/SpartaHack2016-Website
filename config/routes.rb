82cd8a12503cafd4f8ee268bff3bc0ed064bd2c1
Bogdan Pozderca <Bogdan@bogdans-mbp.bluemedora.localnet>
2015-08-11 15:21:20 -0400

    sponsor page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post 'auth'  => 'users#auth'
   get 'app'  => 'users#app'
   post 'save' => 'users#save'
+  get 'sponsor' => 'pages#sponsor'
   resources :users
   
 
