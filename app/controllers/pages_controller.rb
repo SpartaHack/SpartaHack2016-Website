cd0b223bd954c89be7f9e036c97aff28d11d9c31
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-08-22 20:44:14 -0400

    subscribes person to our mailing list, shows message asking for confirmation email

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,21 @@
 class PagesController < ApplicationController
+	require 'mailchimp'
+
   def index
   	render layout: false
   end
 
+  def subscribe 
+  	render layout: false
+  	mailchimp = Mailchimp::API.new(ENV["MAILCHIMP_API_KEY"])
+		mailchimp.lists.subscribe(ENV["MAILCHIMP_LIST_ID"], 
+		                   { "email" => subscribe_params['emailinput']
+		                   },{'FNAME' => subscribe_params['fname'] , "LNAME" => subscribe_params['lname'] })
+	end
+
+  private
+
+  def subscribe_params
+    params.permit(:emailinput, :fname, :lname)
+  end		
 end
