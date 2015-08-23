86e7c5fd0c31153ad976f4b844defe9f46ba33b1
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-08-22 22:30:28 -0400

    Revert "testing mailchimp"
    This reverts commit 1c5ae0942d135bb0e972faf660496d5702de94bb.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,12 +7,10 @@ class PagesController < ApplicationController
 
   def subscribe 
   	render layout: false
-    puts ENV["MAILCHIMP_API_KEY"]
-    puts "MAILCHIMP_LIST_ID"
-  # 	mailchimp = Mailchimp::API.new(ENV["MAILCHIMP_API_KEY"])
-		# mailchimp.lists.subscribe(ENV["MAILCHIMP_LIST_ID"], 
-		#                    { "email" => subscribe_params['emailinput']
-		#                    },{'FNAME' => subscribe_params['fname'] , "LNAME" => subscribe_params['lname'] })
+  	mailchimp = Mailchimp::API.new(ENV["MAILCHIMP_API_KEY"])
+		mailchimp.lists.subscribe(ENV["MAILCHIMP_LIST_ID"], 
+		                   { "email" => subscribe_params['emailinput']
+		                   },{'FNAME' => subscribe_params['fname'] , "LNAME" => subscribe_params['lname'] })
 	end
 
   private
