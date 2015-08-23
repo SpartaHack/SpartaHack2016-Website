cdfad48148a0a73199461189fcc3576c2e5ebf03
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-08-22 21:21:55 -0400

    typo, shaking my head

--- lib/parse_config.rb
+++ lib/parse_config.rb
@@ -1,4 +1,4 @@
 require 'parse-ruby-client'
 
-Parse.init :application_id => EVN["PARSE_APP_ID"],
-    :api_key        => EVN["PARSE_API_KEY"]
+Parse.init :application_id => ENV["PARSE_APP_ID"],
+    :api_key        => ENV["PARSE_API_KEY"]
