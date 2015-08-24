00d568da9f46c05c2ad983dab2bffbdeefa84bef
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-08-23 22:55:25 -0400

    why you no work

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ server '104.131.101.186', port: 22, roles: [:web, :app, :db], primary: true
 set :use_sudo, false
 set :repo_url,        'git@github.com:SpartaHack/SpartaHackII-Web.git'
 set :branch,          "development"
-set :application,     'SpartaHack2016-Dev'
+set :application,     'SpartaHack2016-Apps'
 set :user,            'dev'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
@@ -15,11 +15,11 @@ set :stage,           :production
 set :branch,          :development
 set :deploy_via,      :copy
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://home/dev/apps/SpartaHack2016-Dev/shared/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "unix://home/dev/apps/SpartaHack2016-Apps/shared/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
