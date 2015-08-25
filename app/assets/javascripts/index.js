755bc1136085dd19a53fe19c6e748cafc4f2c327
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-08-25 01:55:41 -0400

    added mobile element to nav bar

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -18,6 +18,7 @@ $('.anchorLink').click(function(){
 var desktop_menu = [
   {"scroll_to": "#home-nav", "elem": $("#hero")},
   {"scroll_to": "#faq-nav", "elem": $("#faq")},
+  {"scroll_to": "#mobile-nav", "elem": $("#mobile")},
   {"scroll_to": "#contact-nav", "elem": $("#contact")},
 ];
 
