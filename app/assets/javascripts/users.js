61b9fd40b09357138e0a899ba1770eb7b9b430d3
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-08-26 01:01:04 -0400

    mobile formatting

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -17,3 +17,24 @@ $(document).ready(function() {
 	});
 })
 
+if ($(window).width() < 321) {
+	$('#title-desc').css('display', 'none');
+	$('#title-hashtag').css('display', 'none');
+	if ($(window).height() < 550) {
+		$('.logo').css('display', 'none');
+	}
+
+}
+
+
+if ($(window).width() < 550) {
+  $("#header").headroom({
+    "offset": 205,
+    "tolerance": 5,
+    "classes": {
+      "initial": "animated",
+      "pinned": "slideDown",
+      "unpinned": "slideUp"
+    }
+  });
+}
