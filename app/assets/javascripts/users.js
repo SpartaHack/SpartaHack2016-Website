a00c0cc61cd1402a551e1098cc6580531f4471ae
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-10-07 18:31:11 -0400

    added events on click of inputs and added major and hackathon

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,24 +1,58 @@
-$(document).ready(function() {
+$(document).ready(function() {	
 	$('#university').select2({
-	  placeholder: "College/University", 
+	  placeholder: "What university do you attend?", 
 	  allowClear: true
 	});
 	$('#gender').select2({
 	  placeholder: "Gender", 
-	  allowClear: true
+	  allowClear: true,
 	});
 	$('#major').select2({
-	  placeholder: "Major(s)", 
+	  placeholder: "Field of study", 
 	  allowClear: true
 	});
 	$('#gradeLevel').select2({
-	  placeholder: "Year in School", 
+	  placeholder: "Year of school", 
 	  allowClear: true
 	});
 	$('#hackathons').select2({
-	  placeholder: "What hackathons have you attended", 
+	  placeholder: "What other MLH hackathons have you attended?", 
 	  allowClear: true
 	});
+
+	$('b[role="presentation"]').hide();
+	$('.select2-selection__arrow').append('<i class="fa fa-angle-down"></i>');	
+	$('.select2-container--open').append('<i class="fa fa-angle-up"></i>');	
+
+	$("#github").click(function() {
+		if ($("#github").val() === "") {
+			$("#github").val('http://www.github.com/');
+		}
+	})
+	$("#linkedIn").click(function() {
+		if ($("#linkedIn").val() === "") {
+			$("#linkedIn").val('https://www.linkedin.com/in/');
+		}
+	})
+
+	$("#devPost").click(function() {
+		if ($("#devPost").val() === "") {
+			$("#devPost").val('http://www.devpost.com/');
+		}
+	})
+
+	$("#website").click(function() {
+		if ($("#website").val() === "") {
+			$("#website").val('http://www.');
+		}
+	})
+
+	$("#coolLink").click(function() {
+		if ($("#coolLink").val() === "") {
+			$("#coolLink").val('http://');
+		}
+	})
+
 })
 
 if ($(window).width() < 321) {
