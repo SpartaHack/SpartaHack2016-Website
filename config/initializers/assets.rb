f87e8c71376aa666f3b49734df6969946440416e
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-10-11 12:07:00 -0400

    added specific files instead of all

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
+Rails.application.config.assets.precompile += %w( select2-modified.css )
+Rails.application.config.assets.precompile += %w( site.css )
+Rails.application.config.assets.precompile += %w( users.css )
+Rails.application.config.assets.precompile += %w( jquery-1.11.3.min.js )
+Rails.application.config.assets.precompile += %w( headroom.min.js )
+Rails.application.config.assets.precompile += %w( jquery-headroom.js )
+Rails.application.config.assets.precompile += %w( index.js )
+Rails.application.config.assets.precompile += %w( users.js )
+Rails.application.config.assets.precompile += %w( app.js )
