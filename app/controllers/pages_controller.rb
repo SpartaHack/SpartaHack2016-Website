f58d294ad7289aef1cd75d821334bf4e692b6def
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-10-23 01:34:37 -0400

    query parse for sponsors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,29 @@
 class PagesController < ApplicationController
 	require 'mailchimp'
+  require 'parse_config'
+  require 'monkey_patch'
 
   def index
-  	render layout: false
+    @partner = []
+    @trainee = []
+    @warrior = []
+    @commander = []
+
+    companies = Parse::Query.new("Company").get
+
+    companies.each do |c|
+      if c["level"] = "partner"
+        @partner.push([c["url"], c["img"].url, c["name"]])
+      elsif c["level"] = "trainee"
+        @trainee.push([c["url"], c["img"].url, c["name"]])
+      elsif c["level"] = "warrior"
+        @warrior.push([c["url"], c["img"].url, c["name"]])
+      elsif c["level"] = "commander"
+        @commander.push([c["url"], c["img"].url, c["name"]])    
+      end
+    end
+
+    render layout: false
   end
 
   def subscribe 
@@ -13,9 +34,10 @@ class PagesController < ApplicationController
 		                   },{'FNAME' => subscribe_params['fname'] , "LNAME" => subscribe_params['lname'] })
 	end
 
+
   private
 
   def subscribe_params
     params.permit(:emailinput, :fname, :lname)
-  end		
+  end
 end
