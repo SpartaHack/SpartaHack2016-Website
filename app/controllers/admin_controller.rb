2625cb4a62a98c955243b43d481b951b2f1e492e
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-10-26 20:43:07 -0400

    editing and deleting sponsors

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   require 'mailchimp'
   require 'parse_config'
   require 'monkey_patch'
-  
+
   def admin
     if cookies.signed[:spartaUser]
       user = Parse::Query.new("_User").eq("objectId", cookies.signed[:spartaUser]).get.first
@@ -12,6 +12,15 @@ class AdminController < ApplicationController
     else
       redirect_to '/login'
     end
+
+    @sponsors = []
+
+    companies = Parse::Query.new("Company").get
+
+    companies.each do |c|
+        @sponsors.push([c["objectId"], c["name"]])
+    end
+
     render layout: false
   end
 
@@ -24,7 +33,7 @@ class AdminController < ApplicationController
       :content_type => logo.content_type,
       :content_length => logo.tempfile().size().to_s
     })
-    print photo.save
+    photo.save
 
     company = Parse::Object.new("Company")
     company['name'] = add_sponsor_params['name']
@@ -38,10 +47,56 @@ class AdminController < ApplicationController
 
   end
 
+  def viewsponsor
+    object = view_sponsor_params['object']
+    @sponsor = Parse::Query.new("Company").eq("objectId", object).get[0]
+
+    render layout: false
+
+  end
+
+  def editsponsor 
+    company = Parse::Query.new("Company").eq("objectId", edit_sponsor_params['object']).get.first
+    
+    if edit_sponsor_params["commit"] == "Delete"
+      company.parse_delete
+    else
+
+      if edit_sponsor_params["picture"]
+        logo = edit_sponsor_params['picture']
+        photo = Parse::File.new({
+          :body => logo.read,
+          :local_filename => logo.original_filename,
+          :content_type => logo.content_type,
+          :content_length => logo.tempfile().size().to_s
+        })
+        photo.save
+        company['img'] = photo
+      end
+
+      company['name'] = edit_sponsor_params['name']
+      company['url'] = edit_sponsor_params['url']
+      company['level'] = edit_sponsor_params['level']
+
+      company.save
+    end
+
+    redirect_to '/admin'
+
+  end
+
   private
 
   def add_sponsor_params
     params.permit(:picture, :name, :url, :level)
+  end   
+
+  def edit_sponsor_params
+    params.permit(:picture, :name, :url, :level, :commit, :object)
+  end   
+
+  def view_sponsor_params
+    params.permit(:object)
   end   
 
 end
