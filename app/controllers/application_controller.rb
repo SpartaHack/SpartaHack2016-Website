c0e901dad199932659ad6899fa16d5e6ecdbe84d
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-11-30 01:47:10 -0500

    sets variable to true if js is enabled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  JAVASCRIPT_TIME_LIMIT = 30.seconds
+
+  before_filter :prepare_javascript_test
+
+  private
+  def prepare_javascript_test
+    if (session[:javascript_updated].blank? or Time.now - Time.parse(session[:javascript_updated]) > ApplicationController::JAVASCRIPT_TIME_LIMIT)
+      @javascript_active = false
+    else
+      @javascript_active = true
+    end
+    
+  end
+
 end
