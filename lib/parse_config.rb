9afd0523f1b1f6cd3bd2d2beae06f015f4c1d9e3
Bogdan Pozderca <bogdanpozderca@users.noreply.github.com>
2015-12-01 16:44:49 -0500

    Now points to dev parse

--- lib/parse_config.rb
+++ lib/parse_config.rb
@@ -1,4 +1,4 @@
 require 'parse-ruby-client'
 
-Parse.init :application_id => ENV["PARSE_APP_ID"],
-    :api_key        => ENV["PARSE_API_KEY"]
+Parse.init :application_id => ENV["PARSE_APP_ID_DEV"],
+    :api_key        => ENV["PARSE_API_KEY_DEV"]
