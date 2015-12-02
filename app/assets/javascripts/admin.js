06ffa1ca1f9496bb398749a5d35fdd3b8343d7f8
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-12-02 04:16:15 -0500

    removed sponsrship js, moved to sponsorship.js

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,25 +1,5 @@
-function readURL(input) {
-	if (input.files && input.files[0]) {
-	  var reader = new FileReader();
 
-	  reader.onload = function (e) {
-	    $('#img_prev')
-	      .attr('src', e.target.result)
-	  };
-
-	  reader.readAsDataURL(input.files[0]);
-	}
-}
 $(document).ready(function() {	
-	$('#level').select2({
-	  placeholder: "Level of Sponsorship", 
-	  allowClear: true
-	});
-
-	$('b[role="presentation"]').hide();
-	$('.select2-selection__arrow').append('<i class="fa fa-angle-down"></i>');	
-	$('.select2-container--open').append('<i class="fa fa-angle-up"></i>');	
-
 
 	if ($(window).width() < 550) {
 	  $("#header").headroom({
