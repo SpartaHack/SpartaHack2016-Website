ed2f836a6ba6846dfc922b5ca9b3a71febeb01fa
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-01-05 15:45:33 -0500

    clean up

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -346,18 +346,14 @@ class AdminController < ApplicationController
       end
     end
     @common_words = most_common(@master_string)
-    # p @common_words
 
-    # p "\n-----------------------------\n\n\n\n"
-    most_common_words = ["the", "be", "to", "of", "and", "a", "in", "that", "have", "i", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", "about", "who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know", "take", "person", "into", "year", "your", "good", "some", "could", "them", "see", "other", "than", "then", "now", "look", "only", "come", "its", "over", "think", "also", "back", "after", "use", "two", "how", "our", "work", "first", "well", "way", "even", "new", "want", "because", "any", "these", "give", "day", "most", "us", "im", "ive", "id"]
+    most_common_words = ["the", "be", "to", "of", "and", "a", "in", "that", "have", "i", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", "about", "who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know", "take", "person", "into", "year", "your", "good", "some", "could", "them", "see", "other", "than", "then", "now", "look", "only", "come", "its", "over", "think", "also", "back", "after", "use", "two", "how", "our", "work", "first", "well", "way", "even", "new", "want", "because", "any", "these", "give", "day", "most", "us", "im", "ive", "id", "am"]
 
     most_common_words.each do |word|
-      p @common_words.delete(word)
+      @common_words.delete(word)
     end
 
     @common_words = @common_words.sort_by {|_key, value| value}.reverse
-
-    p @common_words
 
   end
 
