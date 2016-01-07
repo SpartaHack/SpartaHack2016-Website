262e221033379c75dd211bbd4660b1a063dc8400
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-01-07 15:39:45 -0500

    status changes

---
+++ app/assets/javascripts/users-dashboard.js
@@ -0,0 +1,39 @@
+function confirmJavascript() {
+  $.ajax({
+    url: "/javascript/confirm",
+    context: document.body
+  })
+}
+
+$(document).ready( function () {
+    confirmJavascript();
+    setInterval(confirmJavascript, 10000); // invoke each 10 seconds
+
+	if ($(window).width() < 775) {
+	  $("#header").headroom({
+	    "offset": 205,
+	    "tolerance": 5,
+	    "classes": {
+	      "initial": "animated",
+	      "pinned": "slideDown",
+	      "unpinned": "slideUp"
+	    }
+	  });
+	}
+
+	$(function() {
+	    var pull        = $('#pull');
+	        menu        = $('.mobile');
+	 
+	    $(pull).on('click', function(e) {
+	        e.preventDefault();
+	        menu.slideToggle();
+	    });
+
+	    $('.mobile li a').on('click', function(e) {
+	        menu.slideToggle();
+	    });
+
+	});
+
+});
