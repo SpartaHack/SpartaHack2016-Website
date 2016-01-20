1bc0da86768ad79eb43dd2e7b0b078aa1f2a91c6
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-01-20 01:32:30 -0500

    combining register with application for registration

--- app/assets/javascripts/register.js
+++ app/assets/javascripts/register.js
@@ -1,23 +1,2 @@
 var password = document.getElementById("password")
   , confirm_password = document.getElementById("password_confirmation");
-
-function validatePassword(){
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
-  } else {
-    confirm_password.setCustomValidity('');
-  }
-}
-
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
-
-function confirmJavascript() {
-  $.ajax({
-    url: "/javascript/confirm",
-    context: document.body
-  })
-}
-
-confirmJavascript();
-setInterval(confirmJavascript, 10000); // invoke each 10 seconds
