63bd02fd9cb34cd2f6c3a76a23a97d004133f8f7
Bogdan Pozderca <bogdanpozderca@gmail.com>
2016-02-04 15:30:17 -0500

    rsvp data

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,7 @@
 class ApiController < ApplicationController
   require 'parse_config'
-  
+  require 'json'
+
   def school
     # Collects all the applications for that school
     @apps = Parse::Query.new("Application").tap do |q|
@@ -21,12 +22,32 @@ class ApiController < ApplicationController
     	@school_dates.push(app["createdAt"])
     end
 
-    @response["timestamps"] = @school_dates
+    @rsvps = Parse::Query.new("RSVP").tap do |q|
+    				          q.eq("university", school_params[:school])
+                      q.limit = 1000
+                    end.get
+
+    @rsvps += Parse::Query.new("RSVP").tap do |q|
+    				          q.eq("university", school_params[:school])
+                      q.limit = 1000
+                      q.skip = 1000
+                    end.get
+
+    pp @rsvps
+
+    @rsvp_dates = []
+    @rsvps.each do |rsvp|
+    	@rsvp_dates.push(rsvp["createdAt"])
+    end
+
+    @response["applicant-timestamps"] = @school_dates
     @response["applicant-total"] = @apps.length
-    
+    @response["rsvp-timestamps"] = @rsvp_dates
+    @response["rsvp-total"] = @rsvps.length
+
     respond_to do |format|
-      format.html { render json: @response}
-      format.json { render json: @response}
+      format.html { render json: @response.to_json}
+      format.json { render json: @response.to_json}
  	  end
 
   end
@@ -35,5 +56,5 @@ class ApiController < ApplicationController
 
   def school_params
   	params.permit(:school)
-  end 
+  end
 end
