84e102ea9dafd459a43ad9fa7cb97ab456c5086d
Bogdan Pozderca <bogdanpozderca@gmail.com>
2017-10-10 18:06:04 -0400

    Work

--- lib/parse_config.rb
+++ lib/parse_config.rb
@@ -2,4 +2,4 @@ require 'parse-ruby-client'
 
 Parse.init :application_id => ENV["PARSE_APP_ID"],
     :api_key        => ENV["PARSE_API_KEY"],
-host                => 'http://localhost:1337'
+    :host           => 'http://localhost:1337'
