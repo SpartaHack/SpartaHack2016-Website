5c7c61f7d8f24f71575fc90f78b6bc25d5effb48
Bogdan Pozderca <bogdan.pozderca@gmail.com>
2018-05-17 01:21:31 -0400

    parse host change

--- lib/parse_config.rb
+++ lib/parse_config.rb
@@ -3,4 +3,4 @@ require 'parse-ruby-client'
 $client = Parse.create :application_id => ENV["PARSE_APP_ID"],
     :api_key        => ENV["PARSE_API_KEY"],
     :master_key        => ENV["PARSE_APP_M"],
-    :host           => 'http://localhost:1337'
+    :host           => ENV["PARSE_HOST"]
